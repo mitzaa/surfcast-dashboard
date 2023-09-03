986fd9cae644637858f31a2e3071344ad58314a5
Marion <marionmillard@outlook.com>
2023-09-04 01:32:31 +1200

    fixing console 404 errors on frontend

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,60 @@
 from flask import Flask, jsonify, request, render_template
 import requests
 import os
+import psycopg2
 
 app = Flask(__name__, 
             static_folder='frontend/static', 
             template_folder='frontend/templates')
 
+DATABASE_CONFIG = {
+    'dbname': 'weather',
+    'user': 'marion',
+    'password': 'surfcast',
+    'host': 'localhost',
+    'port': '5432' 
+}
+
+def get_db_connection():
+    conn = psycopg2.connect(**DATABASE_CONFIG)
+    return conn
+
+def create_table():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        '''
+CREATE TABLE IF NOT EXISTS surf_data (
+    id SERIAL PRIMARY KEY,
+    lat FLOAT NOT NULL,
+    lon FLOAT NOT NULL,
+    waveHeight FLOAT,
+    wind FLOAT,
+    swell FLOAT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+        '''
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def save_to_db(lat, lon, waveHeight, wind, swell):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO surf_data (lat, lon, waveHeight, wind, swell) VALUES (%s, %s, %s, %s, %s)",
+        (lat, lon, waveHeight, wind, swell)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
 @app.route('/')
 def index():
     return render_template('index.html')  
 
-@app.route('/surfcast', methods=['GET'])
+@app.route('/surfcast/', methods=['GET'])
 def get_surfcast():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
@@ -35,11 +79,24 @@ def get_surfcast():
     if response.status_code != 200:
         return jsonify({"error": "Failed to fetch data from Stormglass API."}), 500
     
+    data = response.json()
+    
+    waveHeight = data.get('waveHeight')
+    wind = data.get('windSpeed')
+    swell = data.get('swellHeight')
+
+    save_to_db(lat, lon, waveHeight, wind, swell)
+    print(response.status_code)
+    print(response.json())
+
+    
     return jsonify(response.json())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-    print("STORMGLASS_API_KEY:", os.environ.get('STORMGLASS_API_KEY'))
+    app.run(debug=True)
+
+    # host='0.0.0.0
+
 
 
 
--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,18 @@
 -- SQL initialization script
 -- Will add tables and data here
 
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    latitude FLOAT,
+    longitude FLOAT,
+    description TEXT
+);
+
+CREATE TABLE forecasts (
+    id SERIAL PRIMARY KEY,
+    location_id INTEGER REFERENCES locations(id),
+    timestamp TIMESTAMP,
+    waveHeight FLOAT,
+    waveDirection FLOAT,
+);
